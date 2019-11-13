d3eed498c7cbf1d9495b8d859487dc22cf33f115
Kate Campione <katecampione@gmail.com>
2019-11-12 22:00:39 -0700

    change header location

--- routes/form.php
+++ routes/form.php
@@ -11,5 +11,5 @@ $headers = "From: ".$mailFrom;
 $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
 mail($mailTo, $subject, $txt, $headers);
-header("Location: index.php?mailsend");
+header("Location: contact.html?mailsend");
 }
