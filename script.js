a1353777b3ce32d666b49c0bf76e572787497f77
Kate Campione <katecampione@gmail.com>
2021-05-14 16:13:11 -0600

    typewriter javascript

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
-    strings:['Hi', 'my name is Kate.']
+    strings: ['Hi my name is Kate.', 'I am a skilled Shopify Developer.'],
     autoStart: true,
-    loop: true
-});
-
-typewriter.typeString('Hello World!')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('Strings can be removed')
-    .pauseFor(2500)
-    .deleteChars(7)
-    .typeString('<strong>altered!</strong>')
-    .pauseFor(2500)
-    .start();
+    loop: true,
+  });
