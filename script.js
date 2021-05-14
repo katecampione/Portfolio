8258398481fbef7c87fae3b8671b46f1f37b3289
Kate Campione <katecampione@gmail.com>
2021-05-14 16:19:25 -0600

    typewriter script

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
-    strings: ['Hi my name is Kate.', 'I am a skilled Shopify Developer.'],
+    strings: ['Hi, my name is Kate! I am a skilled Shopify Developer.'],
+    strings: ['I have a passion for adventure and BIG ideas!'],
     autoStart: true,
     loop: true,
   });
