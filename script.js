16ae26e925992bcddb9d366d7c8fe1482a4f192c
Kate Campione <katecampione@gmail.com>
2021-05-14 17:18:14 -0600

    add pic to homepage

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var app = document.getElementById('app');
 
 var typewriter = new Typewriter(app, {
-    strings: ['Hi, my name is Kate! I am a skilled Shopify Developer.'],
-    strings: ['I have a passion for adventure and BIG ideas!'],
-    autoStart: true,
     loop: true,
   });
+
+typewriter.typeString('Hi, my name is Kate!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('I am a skilled Shopify Developer...')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('with a passion for adventure & <strong>BIG</strong> ideas!')
+    .pauseFor(2500)
+    .start();
